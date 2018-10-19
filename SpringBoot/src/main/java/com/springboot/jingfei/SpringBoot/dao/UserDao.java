daa0d71b50d89c45fc7447d6164ddc65b1dbb7a9
jingfei <743793426@qq.com>
2018-10-19 18:30:10 +0800

    整合oracle数据库

--- SpringBoot/src/main/java/com/springboot/jingfei/SpringBoot/dao/UserDao.java
+++ SpringBoot/src/main/java/com/springboot/jingfei/SpringBoot/dao/UserDao.java
@@ -2,10 +2,12 @@ package com.springboot.jingfei.SpringBoot.dao;
 
 import com.springboot.jingfei.SpringBoot.bean.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface UserDao {
     List<User> getUserList();
 }
