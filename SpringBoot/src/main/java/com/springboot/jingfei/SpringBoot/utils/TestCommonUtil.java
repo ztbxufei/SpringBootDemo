b34e205f9a8d6024065773b831ddf03e2fded7a5
xufei <18855974469@163.com>
2018-11-29 18:07:53 +0800

    222

---
+++ SpringBoot/src/main/java/com/springboot/jingfei/SpringBoot/utils/TestCommonUtil.java
@@ -0,0 +1,55 @@
+package com.springboot.jingfei.SpringBoot.utils;
+
+/**
+ * @Author xf
+ * @Date Created in 17:44 2018/11/29
+ */
+public class TestCommonUtil {
+
+
+      public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+     }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+    public static void main (String[] args){
+        ListNode l1 = new ListNode(0);
+        ListNode l = l1;
+        l.next = new ListNode(2);
+        l = l.next;
+        l.next = new ListNode(4);
+        l = l.next;
+        l.next = new ListNode(5);
+
+
+        ListNode l2 = new ListNode(0);
+        ListNode s = l2;
+        s.next = new ListNode(2);
+        s = s.next;
+        s.next = new ListNode(4);
+        s = s.next;
+        s.next = new ListNode(5);
+
+        System.out.println(addTwoNumbers(l,l2).val);
+
+    }
+}
