e53eaa8b235c1870dd57456d95e6991c0eb035fd
jingfei <743793426@qq.com>
2019-01-09 17:30:34 +0800

    版本

--- SpringBoot/src/main/java/com/springboot/jingfei/SpringBoot/bean/xml/Field.java
+++ SpringBoot/src/main/java/com/springboot/jingfei/SpringBoot/bean/xml/Field.java
@@ -1,6 +1,5 @@
 package com.springboot.jingfei.SpringBoot.bean.xml;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
