b101952544e4ff83cded129fe1136749a1171c7b
ssaorry <743793426@qq.com>
2019-01-23 21:12:35 +0800

    删除框架搭建

---
+++ SpringBoot/src/main/java/com/springboot/jingfei/SpringBoot/dao/DataBaseDao.java
@@ -0,0 +1,15 @@
+package com.springboot.jingfei.SpringBoot.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface DataBaseDao {
+    int insert(Map map);
+
+    List<Map> selectList(Map map);
+
+    int delete(Map map);
+}
